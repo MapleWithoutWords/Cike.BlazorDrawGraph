858df65fbeeefebb01f0aa0d96ff03e2d1775504
liming <1028139084@qq.com>
2023-03-22 00:43:33 +0800

    init project

---
+++ src/Cike.DrawGraph/Basic/BRectangle.cs
@@ -0,0 +1,14 @@
+﻿namespace Cike.DrawGraph.Basic
+{
+    public class BRectangle : BPoint
+    {
+        public BRectangle(int x, int y, int width, int height) : base(x, y)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
